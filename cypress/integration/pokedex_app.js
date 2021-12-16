4ac08a84b2ddf43c1cfadb14824af8a70067b4e3
Mikko Naalisvaara <mikko.naalisvaara@gmail.com>
2021-12-16 09:21:20 +0200

    Add exercise 11.9: Simple end to end tests

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('navigating to a single Pokemon page works', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
