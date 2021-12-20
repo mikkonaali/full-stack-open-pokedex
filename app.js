da594163720cc4eef8d97b1a52929b51635e22eb
Mikko Naalisvaara <mikko.naalisvaara@gmail.com>
2021-12-20 14:34:57 +0200

    Test scheduled health check

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
@@ -14,7 +14,7 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version is deployed
 })
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
-})
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log('server started on port 5000')
+// })
